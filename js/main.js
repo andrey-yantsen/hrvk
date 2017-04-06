7a1e1913b165042271ac5ff5f7bfef64a644e227
Andrey 'virus' Yantsen <andrey@janzen.su>
2017-04-07 00:57:41 +0300

    improve the interface

--- js/main.js
+++ js/main.js
@@ -1,146 +1,347 @@
-window.onload = function() {
-    $(document).on('click', '#keyword-list span.glyphicon-remove', function() {
-        $(this).parent().remove();
+window.onload = function () {
+    $("#age-range").slider({
+        tooltip: 'always',
+        selection: 'after',
+        tooltip_split: true
     });
 
-    $(document).on('click', '#keyword-list span.glyphicon-plus', function() {
-        var $li = $(this).parent();
-        var $clone = $li.clone();
-        $('input', $clone).val('');
-        $clone.insertAfter($li);
-    });
+    var countriesMap = {};
 
-    $('#refresh_groups').click(function() {
-        var $ul = $('#groups');
-        var addedGroups = [];
-        $ul.html('');
-        var delay = 0;
-
-        $('#keyword-list input').each(function() {
-            var value = this.value;
-            if (value) {
-                setTimeout(function() {
-                    VK.api('groups.search', {q: value, type: 'group', count: 15}, function(data) {
-                        var items = data.response.items;
-
-                        for (var key in items) {
-                            if (items.hasOwnProperty(key)) {
-                                var group = items[key];
-                                var group_id = group.id.toString();
-
-                                if (addedGroups.indexOf(group_id) != -1) {
-                                    continue;
-                                } else {
-                                    addedGroups.push(group_id);
-                                }
-
-                                var html = '<li><input type="checkbox" name="group[]" value="' + group_id + '" checked/> ' +
-                                    '<a href="https://vk.com/public' + group_id + '" target="_blank">' + group.name + '</a></li>';
-                                $ul.append(html);
-                            }
-                        }
-                    });
-                }, delay);
-                delay += 350;
+    VK.init(function () {
+        VK.api('database.getCountries', {
+            need_all: 1,
+            count: 1000
+        }, function (data) {
+            for (v in data.response.items) {
+                countriesMap[data.response.items[v].title] = data.response.items[v].id;
             }
         });
-        return false;
-    });
+    }, function () {
+        alert('Something failed, sorry');
+    }, '5.62');
 
-    var tt = null;
-    var users = {};
+    var $refreshGroupsGI = $('#refresh-groups');
 
-    $('#begin').click(function() {
-        var age_from = document.getElementById('age_from').value;
-        var age_to = document.getElementById('age_to').value;
-        var delay = 0;
-        $('input[name="group[]"]:checked').each(function () {
-            var group_id = this.value;
-            $('#cities_select :selected').each(function() {
-                var city_id = this.value;
+    var refreshBtnAddClassGen = function (className) {
+        return function (event) {
+            $refreshGroupsGI.addClass(className);
+        };
+    };
 
-                setTimeout(function() {
-                    VK.api('users.search', {
-                        city: city_id,
-                        sex: 2,
-                        age_from: age_from,
-                        age_to: age_to,
-                        group_id: group_id,
-                        count: 1000
-                    }, function(data) {
-                        var items = data.response.items;
-                        for (var key in items) {
-                            if (items.hasOwnProperty(key)) {
-                                var user = items[key];
-                                if (users.hasOwnProperty(user.id)) {
-                                    users[user.id].groups.push(group_id);
-                                } else {
-                                    user.groups = [group_id];
-                                    users[user.id] = user;
-                                }
-                            }
-                        }
-                        if (items.length > 0) {
-                            if (tt) {
-                                clearTimeout(tt);
-                            }
-                            tt = setTimeout(display, 700);
-                        }
-                    });
-                }, delay);
-                delay += 350;
+    $('#keyword-list').on('itemAdded', refreshBtnAddClassGen('pending'))
+        .on('itemRemoved', refreshBtnAddClassGen('pending'));
+
+    $refreshGroupsGI.on('click', function() {
+            $refreshGroupsGI.addClass('spin');
+            var $ul = $('#groups-list');
+            var addedGroups = [];
+            var delay = 0;
+            $ul.html('');
+
+            var code = [];
+
+            $.each($("#keyword-list").tagsinput('items'), function(idx, value) {
+                code.push('API.groups.search(' + JSON.stringify({q: value, type: 'group', count: 10}) + ')');
             });
+
+            if (code.length === 0) {
+                $refreshGroupsGI.removeClass('spin pending');
+                return false;
+            }
+
+            VK.api('execute', {'code': 'return [' + code.join(',') + '];'}, function (data) {
+                for (var idx in data.response) {
+                    var items = data.response[idx].items;
+                    for (var key in items) {
+                        if (items.hasOwnProperty(key)) {
+                            var group = items[key];
+                            var group_id = group.id.toString();
+
+                            if (addedGroups.indexOf(group_id) !== -1) {
+                                continue;
+                            } else {
+                                addedGroups.push(group_id);
+                            }
+
+                            var html = '<li><input type="checkbox" name="group[]" value="' + group_id + '" checked/> ' +
+                                '<a href="https://vk.com/public' + group_id + '" target="_blank">' + group.name + '</a></li>';
+                            $ul.append(html);
+                        }
+                    }
+                }
+                VK.callMethod('resizeWindow', null, $(document).height());
+                $refreshGroupsGI.removeClass('spin pending');
+            });
+
+            return false;
         });
-        return false;
-    });
 
-    function display() {
-        var $ul = $('#response');
-        $ul.html('');
-
-        var sortable = [];
-        for (var user_id in users) {
-            sortable.push([users[user_id], users[user_id].groups.length]);
-        }
-
-        sortable.sort(function(a, b) {
-            return b[1] - a[1];
-        });
-
-        var html = '';
-
-        for (var idx in sortable) {
-            var user = sortable[idx][0];
-            var cnt = sortable[idx][1];
-            if (cnt > 1) {
-                var name = user.last_name + ' ' + user.first_name;
-                html += '<li><a href="https://vk.com/id' + user.id.toString() + '">' + name + '</a> (' + cnt.toString() + ')</li>';
+    var cities = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '%QUERY%',
+            wildcard: '%QUERY%',
+            transport: function (query, onSuccess, onError) {
+                var term = decodeURIComponent(query.url);
+                var countryName = $('#country').val();
+                if (!countriesMap.hasOwnProperty(countryName)) {
+                    alert('Unknown country, sorry :(');
+                    return false;
+                }
+                var countryId = countriesMap[countryName];
+                VK.api('database.getCities', {country_id: countryId, q: term, need_all: 1, count: 50}, function(data) {
+                    onSuccess(data.response.items);
+                });
             }
         }
+    });
 
-        $ul.html(html);
+    var $elt = $('#cities-list');
+    $elt.tagsinput({
+        itemValue: 'id',
+        itemText: 'title',
+        typeaheadjs: {
+            name: 'cities',
+            displayKey: 'title',
+            source: cities.ttAdapter()
+        }
+    });
 
-        return null;
-    }
+    var school = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '%QUERY%',
+            wildcard: '%QUERY%',
+            transport: function (query, onSuccess, onError) {
+                var term = decodeURIComponent(query.url);
+                var code = [];
+                var cities = {};
 
-    VK.init(function() {
-        window.success = true;
+                $.each($('#cities-list').tagsinput('items'), function(key, value) {
+                    cities[key] = value.title;
+                    var options = {city_id: value.id, q: term};
+                    code.push('API.database.getSchools(' + JSON.stringify(options) + ')');
+                });
 
-        VK.api('database.getCities', {country_id: 1, count: 1000}, function(data) {
-            var items = data.response.items;
-            var select = document.getElementById('cities_select');
+                VK.api('execute', {code: 'return [' + code.join(',') + '];'}, function(data) {
+                    var schools = [];
+                    for (var idx in data.response) {
+                        var items = data.response[idx].items;
+                        for (var key in items) {
+                            if (items.hasOwnProperty(key)) {
+                                var item = items[key];
+                                schools.push({id: item.id, title: item.title + ', ' + cities[idx]});
+                            }
+                        }
+                    }
+                    onSuccess(schools);
+                });
+            }
+        }
+    });
 
-            for (var key in items) {
-                if (items.hasOwnProperty(key)) {
-                    var city = items[key];
-                    var opt = document.createElement('option');
-                    opt.value = city.id;
-                    opt.innerHTML = city.title;
-                    select.appendChild(opt);
+    var $elt = $('#schools-list');
+    $elt.tagsinput({
+        itemValue: 'id',
+        itemText: 'title',
+        typeaheadjs: {
+            name: 'school',
+            displayKey: 'title',
+            source: school.ttAdapter()
+        }
+    });
+
+    var university = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '%QUERY%',
+            wildcard: '%QUERY%',
+            transport: function (query, onSuccess, onError) {
+                var term = decodeURIComponent(query.url);
+                var code = [];
+                var cities = {};
+
+                $.each($('#cities-list').tagsinput('items'), function(key, value) {
+                    cities[key] = value.title;
+                    var options = {city_id: value.id, q: term};
+                    code.push('API.database.getUniversities(' + JSON.stringify(options) + ')');
+                });
+
+                VK.api('execute', {code: 'return [' + code.join(',') + '];'}, function(data) {
+                    var universities = [];
+                    for (var idx in data.response) {
+                        var items = data.response[idx].items;
+                        for (var key in items) {
+                            if (items.hasOwnProperty(key)) {
+                                var item = items[key];
+                                universities.push({id: item.id, title: item.title + ', ' + cities[idx]});
+                            }
+                        }
+                    }
+                    onSuccess(universities);
+                });
+            }
+        }
+    });
+
+    var $elt = $('#university-list');
+    $elt.tagsinput({
+        itemValue: 'id',
+        itemText: 'title',
+        typeaheadjs: {
+            name: 'university',
+            displayKey: 'title',
+            source: university.ttAdapter()
+        }
+    });
+
+    var faculty = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace
+    });
+
+    var refreshFaculties = function() {
+        faculty.clear();
+        var code = [];
+        var universities = {};
+
+        $.each($('#university-list').tagsinput('items'), function(key, value) {
+            universities[key] = value.title;
+            var options = {university_id: value.id};
+            code.push('API.database.getFaculties(' + JSON.stringify(options) + ')');
+        });
+
+        VK.api('execute', {code: 'return [' + code.join(',') + '];'}, function(data) {
+            for (var idx in data.response) {
+                var items = data.response[idx].items;
+                for (var key in items) {
+                    if (items.hasOwnProperty(key)) {
+                        var item = items[key];
+                        faculty.add({id: item.id, title: item.title + ', ' + universities[idx]});
+                    }
                 }
             }
         });
-    }, function() {
-        alert('Something failed, sorry');
-    }, '5.62');
+    };
+    $elt.on('itemAdded', refreshFaculties).on('itemRemoved', refreshFaculties);
+
+    var $elt = $('#faculty-list');
+    $elt.tagsinput({
+        itemValue: 'id',
+        itemText: 'title',
+        typeaheadjs: {
+            name: 'faculty',
+            displayKey: 'title',
+            source: faculty.ttAdapter()
+        }
+    });
+
+    $('button').click(function() {
+        var code = [];
+        var $ul = $('#users-list');
+        $ul.html('');
+
+        var ages = $("#age-range").slider('getValue');
+
+        var requests = [{
+            sex: $('input[name=sex]:checked').val(),
+            age_from: ages[0],
+            age_to: ages[1],
+            count: 1000
+        }];
+
+        var ret = [];
+        $('input[name="group[]"]:checked').each(function () {
+            for (var r in requests) {
+                var req = JSON.parse(JSON.stringify(requests[r]));
+                req['group_id'] = parseInt(this.value);
+                ret.push(req);
+            }
+        });
+        requests = ret.length ? ret : requests;
+        ret = [];
+        $.each($('#cities-list').tagsinput('items'), function(key, value) {
+            for (var r in requests) {
+                var req = requests[r];
+                req['city'] = value.id;
+                ret.push(req);
+            }
+        });
+        requests = ret.length ? ret : requests;
+        ret = [];
+        $.each($('#schools-list').tagsinput('items'), function(key, value) {
+            for (var r in requests) {
+                var req = requests[r];
+                req['school'] = value.id;
+                ret.push(req);
+            }
+        });
+        requests = ret.length ? ret : requests;
+        ret = [];
+        $.each($('#university-list').tagsinput('items'), function(key, value) {
+            for (var r in requests) {
+                var req = requests[r];
+                req['university'] = value.id;
+                ret.push(req);
+            }
+        });
+        requests = ret.length ? ret : requests;
+        ret = [];
+        $.each($('#faculty-list').tagsinput('items'), function(key, value) {
+            for (var r in requests) {
+                var req = requests[r];
+                req['university_faculty'] = value.id;
+                ret.push(req);
+            }
+        });
+        requests = ret.length ? ret : requests;
+        console.log(requests);
+
+        code = [];
+        for (var r in requests) {
+            code.push('API.users.search(' + JSON.stringify(requests[r]) + ')');
+        }
+        var users = {};
+        VK.api('execute', {code: 'return [' + code.join(',') + '];'}, function (data) {
+            for (var r in data.response) {
+                var items = data.response[r].items;
+                for (var key in items) {
+                    if (items.hasOwnProperty(key)) {
+                        var user = items[key];
+                        if (users.hasOwnProperty(user.id)) {
+                            users[user.id].occurencies++;
+                        } else {
+                            user.occurencies = 1;
+                            users[user.id] = user;
+                        }
+                    }
+                }
+            }
+
+            var sortable = [];
+            for (var user_id in users) {
+                sortable.push([users[user_id], users[user_id].occurencies]);
+            }
+
+            sortable.sort(function (a, b) {
+                return b[1] - a[1];
+            });
+
+            var html = '';
+
+            for (var idx in sortable) {
+                var user = sortable[idx][0];
+                var cnt = sortable[idx][1];
+                var name = user.last_name + ' ' + user.first_name;
+                html += '<li><a href="https://vk.com/id' + user.id.toString() + '">' + name + '</a> (' + cnt.toString() + ')</li>';
+            }
+
+            $ul.html(html);
+            VK.callMethod('resizeWindow', null, $(document).height());
+        });
+        return false;
+    });
 };
